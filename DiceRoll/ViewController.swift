d02729be7c54fda6d5470b586d976ed721d8a88e
HishD <hish2k15@gmail.com>
2021-01-31 13:25:55 +0530

    Added auto-layout constraints

--- DiceRoll/ViewController.swift
+++ DiceRoll/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imgDiceOne: UIImageView!
+    @IBOutlet weak var imgDiceTwo: UIImageView!
+    
+    let diceFaces = [
+        UIImage(systemName: "die.face.1.fill"),
+        UIImage(systemName: "die.face.2.fill"),
+        UIImage(systemName: "die.face.3.fill"),
+        UIImage(systemName: "die.face.4.fill"),
+        UIImage(systemName: "die.face.5.fill"),
+        UIImage(systemName: "die.face.6.fill")]
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        flip()
     }
 
-
+    @IBAction func btnRollClicked(_ sender: UIButton) {
+        flip()
+    }
+    
+    func flip() {
+        imgDiceOne.image = diceFaces[Int.random(in: 0...5)]
+        imgDiceTwo.image = diceFaces[Int.random(in: 0...5)]
+    }
+    
 }
 
